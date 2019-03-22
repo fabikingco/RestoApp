eeedaaa2295752d5b6cb394592f3aaa7761bf0e1
Fabian Ardila <fabiking.co@gmail.com>
2019-03-21 19:21:42 -0500

    Se agregaron editText a la activity Nuevo Producto

--- app/src/main/java/com/wposs/buc/restpapp/Tools.java
+++ app/src/main/java/com/wposs/buc/restpapp/Tools.java
@@ -8,9 +8,7 @@ public class Tools {
     public static void startView(AppCompatActivity activity , Class<?> cls){
         Intent intent = new Intent();
         intent.setClass(activity , cls);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         activity.startActivity(intent);
-        activity.finish();
+        //activity.finish();
     }
 }
