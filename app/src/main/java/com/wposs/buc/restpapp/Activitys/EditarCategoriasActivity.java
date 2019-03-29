70ed8f9623b4069e2201c27bf5c0f47184ad7548
Fabian Ardila <fabiking.co@gmail.com>
2019-03-28 19:11:35 -0500

    Inflar AlertDialog con botones para modificacion de cada categoria.

--- app/src/main/java/com/wposs/buc/restpapp/Activitys/EditarCategoriasActivity.java
+++ app/src/main/java/com/wposs/buc/restpapp/Activitys/EditarCategoriasActivity.java
@@ -1,16 +1,20 @@
 package com.wposs.buc.restpapp.Activitys;
 
 import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.wposs.buc.restpapp.BD.Controler.ClsConexion;
@@ -46,6 +50,25 @@ public class EditarCategoriasActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String categoria = adapter.getItem(position);
                 Toast.makeText(EditarCategoriasActivity.this, "Data seleccionada " + categoria, Toast.LENGTH_SHORT).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(EditarCategoriasActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+                View v = inflater.inflate(R.layout.dialog_bd_botones, null);
+
+                builder.setView(v);
+                final AlertDialog dialog = builder.show();
+
+                TextView btnCancelar = v.findViewById(R.id.btnCancelar);
+                final EditText etCategoria = v.findViewById(R.id.etCategoria);
+
+
+
+                btnCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
             }
         });
     }
