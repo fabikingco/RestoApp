3f3b5d5c8d9b1ba036440f44dc53bad24793be63
Fabian Ardila <fabiking.co@gmail.com>
2019-04-01 16:36:20 -0500

    Nuevo icono y splash

--- app/src/main/java/com/wposs/buc/restpapp/Activitys/MainActivity.java
+++ app/src/main/java/com/wposs/buc/restpapp/Activitys/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
         bd = new ClsConexion(this);
     }
