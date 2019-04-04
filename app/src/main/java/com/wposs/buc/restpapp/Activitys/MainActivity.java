535a0fc5ac8b1925089d197f37080c1ce4d85f31
fabikingco <fabikingco@users.noreply.github.com>
2019-04-03 23:50:10 -0500

    Pedir permisos de SMS para versiones mayores a 6
    Falta configurar cuando la persona no acepta el permiso y realizar las pruebas de envio de SMS.

--- app/src/main/java/com/wposs/buc/restpapp/Activitys/MainActivity.java
+++ app/src/main/java/com/wposs/buc/restpapp/Activitys/MainActivity.java
@@ -1,7 +1,14 @@
 package com.wposs.buc.restpapp.Activitys;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,12 +20,49 @@ public class MainActivity extends AppCompatActivity {
 
     ClsConexion bd;
 
+    private static final String TAG = "MainActivity";
+    private static final int SMS_PERMISSION_CODE = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
         bd = new ClsConexion(this);
+
+        solicitarPermisos();
+    }
+
+    private void solicitarPermisos() {
+       if (!isSmsPermissionGranted()) {
+           showRequestPermissionsInfoAlertDialog();
+       }
+    }
+
+    private void showRequestPermissionsInfoAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Solicitud de permisos de SMS");
+        builder.setMessage("Esta aplicacion tiene una funcion que permite enviar mensajes de texto. Acepte los siguientes permisos para usar esta funcion. ");
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                requestReadAndSendSmsPermission();
+            }
+        });
+        builder.show();
+    }
+
+    public boolean isSmsPermissionGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestReadAndSendSmsPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+            Log.d(TAG, "shouldShowRequestPermissionRationale(), no permission requested");
+            return;
+        }
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.READ_SMS}, SMS_PERMISSION_CODE);
     }
 
     public void crearPedido(View view) {
@@ -31,5 +75,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void configurarRestaurante(View view) {
         Tools.startView(this, ConfigurarRestauranteActivity.class);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case SMS_PERMISSION_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
