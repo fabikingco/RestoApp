a1b60ee4b89d3c1df9adfe51c3e3354822274bdf
Fabian Ardila <fabiking.co@gmail.com>
2019-04-26 15:02:40 -0500

    Creacion del menu para cerrar sesion

--- app/src/main/java/com/wposs/buc/restpapp/Activitys/MainActivity.java
+++ app/src/main/java/com/wposs/buc/restpapp/Activitys/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,14 +31,42 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int SMS_PERMISSION_CODE = 0;
 
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences("login", Context.MODE_PRIVATE);
         bd = new ClsConexion(this);
         tvUsuario = findViewById(R.id.tvUsuario);
         bannerUsuario();
         solicitarPermisos();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.item_CerrarSesion){
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("loginActivo", false);
+            editor.putString("user", "error");
+            editor.putString("name", "error");
+            editor.putString("role", "error");
+            editor.apply();
+
+            Tools.startView(this, LoginActivity.class);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void crearPedido(View view) {
